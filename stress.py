c6be48a1091f0d2c5f7163384f1e2731f6e825d8
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-10-07 23:22:31 -0500

    convert to human readable format

--- stress.py
+++ stress.py
@@ -1,3 +1,4 @@
+import math
 import resource
 
 import numpy
@@ -5,4 +6,15 @@ import numpy
 result = [numpy.random.bytes(1024 * 1024) for _ in range(1024)]
 
 print(len(result))
-print(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
+
+
+def size_converter(byte_size):
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+    integer = int(math.floor(math.log(byte_size, 1024)))
+    power = math.pow(1024, integer)
+    size = round(byte_size / power, 2)
+    response = str(size) + ' ' + size_name[integer]
+    return response
+
+
+print(size_converter(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
