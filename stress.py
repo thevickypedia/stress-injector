d67614836b6b197c6d772dd221978c761e74bc8c
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-10-07 23:39:24 -0500

    maintain coding standards

--- stress.py
+++ stress.py
@@ -1,14 +1,16 @@
-import math
 import resource
 
-import numpy
 
-result = [numpy.random.bytes(1024 * 1024) for _ in range(1024)]
-
-print(len(result))
+def stress():
+    """Generates random bytes with the multiple of 1024 (~1GB)"""
+    import numpy
+    result = [numpy.random.bytes(1024 * 1024) for _ in range(1024)]
+    return f'Injected stress in bytes: {len(result)}'
 
 
 def size_converter(byte_size):
+    """Converts bytes into appropriate readable size"""
+    import math
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
     integer = int(math.floor(math.log(byte_size, 1024)))
     power = math.pow(1024, integer)
@@ -17,4 +19,7 @@ def size_converter(byte_size):
     return response
 
 
-print(size_converter(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
+if __name__ == '__main__':
+    print(stress())
+    memory_utilized = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    print(f'Actual memory consumed: {size_converter(memory_utilized)}')
