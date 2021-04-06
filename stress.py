20f4b9d8e390f2f4aaa1e45e0275ecd6f5548259
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-04-05 21:07:18 -0500

    show each core's max utilization and handle manual interrupts

--- stress.py
+++ stress.py
@@ -1,14 +1,14 @@
-import os
-import platform
-import resource
 import sys
-import time
-from multiprocessing import Process
+from math import log, floor, pow
+from multiprocessing import Process as Multiprocess
+from os import getpid, cpu_count
+from platform import system
+from resource import getrusage, RUSAGE_SELF
 from threading import Thread
+from time import sleep
 
-import numpy
-from psutil import cpu_percent, virtual_memory
-from psutil import Process as Process_
+from numpy.random import bytes
+from psutil import cpu_percent, virtual_memory, Process
 from tqdm import tqdm
 
 kill_signal = False
@@ -17,44 +17,42 @@ kill_signal = False
 def stress(gb):
     """Generates random bytes with the multiple of 1024 (~1GB)"""
     mb2bytes = 1024 * 1024
-    result = [numpy.random.bytes(mb2bytes) for _ in tqdm(range(gb), desc='Generating random bytes', unit=' bytes',
-                                                         leave=False)]
-    return f'Injected stress in bytes: {(len(result) * mb2bytes)}'
+    result = [bytes(mb2bytes) for _ in tqdm(range(gb), desc='Generating random bytes', unit=' bytes',
+                                            leave=False)]
+    return f'Stress Injected: {size_converter(len(result) * mb2bytes)}'
 
 
 def memory_util_check():
     """Returns memory used only the current script."""
-    operating_system = platform.system()
+    operating_system = system()
     if operating_system == 'Darwin':
-        util = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
-        return util
+        return getrusage(RUSAGE_SELF).ru_maxrss
     elif operating_system == 'Windows':
-        process = Process_(os.getpid())
-        util = process.memory_info().peak_wset
-        return util
+        process = Process(getpid())
+        return process.memory_info().peak_wset
 
 
 def size_converter(byte_size):
     """Converts bytes into appropriate readable size."""
-    import math
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
-    integer = int(math.floor(math.log(byte_size, 1024)))
-    power = math.pow(1024, integer)
+    integer = int(floor(log(byte_size, 1024)))
+    power = pow(1024, integer)
     size = round(byte_size / power, 2)
     response = str(size) + ' ' + size_name[integer]
     return response
 
 
 def memory_stress():
-    """Gets user input for the number of GBs stress has to be induced and triggers the memory stress"""
+    """Gets user input for the number of GBs stress has to be induced and triggers the memory stress.
+    Suggests twice the amount of physical memory."""
     current_memory = size_converter(virtual_memory().total)
     desired_bytes = input(f"Enter the number of GBs to stress your memory: "
-                          f"(Optimal for you: {round(float(current_memory.split()[0]) * 5)} GB)\n")
+                          f"(Optimal for you: {round(float(current_memory.split()[0]) * 2)} GB)\n")
     gigabytes = int(desired_bytes) * 1024  # gigabytes to megabytes
     try:
         print(f'Stressing Memory with {desired_bytes} GB')
         print(stress(gigabytes))
-        print(size_converter(memory_util_check()))
+        print(f'Memory Consumed: {size_converter(memory_util_check())}')
     except KeyboardInterrupt:
         pass
 
@@ -62,24 +60,37 @@ def memory_stress():
 def infinite():
     """Infinite loop to stress CPU"""
     while True:
-        pass
+        try:
+            for _ in range(cpu_count()):
+                pass
+        except KeyboardInterrupt:
+            sys.exit(0)
 
 
 def measure_cpu():
     """Uses cpu_percent to get the CPU utilization and prints the max impacted core"""
-    max_percent, max_ = 0, ''
+    processors = []
     while True:
         if kill_signal:
             break
         cpu_util = cpu_percent(interval=1, percpu=True)
+        processors.append(cpu_util)  # stores the list of usage % as a list within a list
         output = ''
         for index, percent in enumerate(cpu_util):
             output += f'Core {index + 1}: {percent}%\t'
-            if max_percent < percent:
-                max_percent = percent
-                max_ = index + 1
         sys.stdout.write(f'\r{output.strip()}')
-    print(f'\nMax induced stress: {int(max_percent)}% on Core {max_}')
+    sys.stdout.write('\r')  # flushes the screen output
+    processors = map(list, zip(*processors))  # transpose matrix, so first list will have all usage % of core1 and so on
+    processors = [max(processor) for processor in processors]  # gets the maximum of each list to convert matrix to list
+    processors = list(enumerate(processors))  # creates a list of index values and CPU usage in a set
+    processors = sorted(processors, key=lambda x: x[1], reverse=True)  # sorts by CPU usage within the set in the list
+
+    def format_number(n):
+        """Converts numbers with float value .0 to integers"""
+        return int(n) if isinstance(n, float) and n.is_integer() else n
+
+    print('CPU Usage:')
+    [print(f'Core {processor + 1} - {format_number(usage)}%') for processor, usage in processors]
 
 
 def cpu_stress():
@@ -87,23 +98,23 @@ def cpu_stress():
     CPU monitor using multiprocessing to notify CPU performance under stress."""
     global kill_signal
     waiter = int(input(f'Enter the number of seconds to stress your CPU cores: '
-                       f'(Optimal for you: {os.cpu_count() * 5} secs)\n'))
+                       f'(Optimal for you: {cpu_count() * 5} secs)\n'))
     try:
         print(f'Stressing CPU cores for {waiter} seconds')
         processes = []
-        for n in range(os.cpu_count()):
-            processes.append(Process(target=infinite))
+        for n in range(cpu_count()):
+            processes.append(Multiprocess(target=infinite))
         measure = Thread(target=measure_cpu)
         [each_core.start() for each_core in processes]
         measure.start()
-        time.sleep(waiter)
+        sleep(waiter)
         [each_core.terminate() for each_core in processes]
         [each_core.join() for each_core in processes]
-        time.sleep(2)
+        sleep(2)
         kill_signal = True
         measure.join()
     except KeyboardInterrupt:
-        pass
+        kill_signal = True
 
 
 if __name__ == '__main__':
